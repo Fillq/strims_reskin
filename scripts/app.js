4829daad1a276443084f12eb0bdd2515d9d9714b
Fillq <filip007p@gmail.com>
2025-05-05 21:38:41 +0200

    GUI
    Added GUI and on/off functionality for better user experience

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,25 @@
-const site = window.location.hostname;
-const injectEditedStyles = (css) => {
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.type = 'text/css';
-    link.href = chrome.runtime.getURL(css);
-    document.head.appendChild(link);
-};
+chrome.storage.sync.get(['reskinEnabled'], (result) => {
+    const injectEditedStyles = (css) => {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.type = 'text/css';
+        link.href = chrome.runtime.getURL(css);
+        document.head.appendChild(link);
+    };
 
-if (site.includes("strimsy.top")) {
-    injectEditedStyles('../css/Edit.css');
-
-    const ligi = document.querySelector('.ligi');
-    if (ligi) {
-        ligi.childNodes.forEach(node => {
-            if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() === "||") {
-                ligi.removeChild(node);
+    if (result.reskinEnabled ?? true) {
+        const site = window.location.hostname;
+        if (site.includes("strimsy.top")) {
+            injectEditedStyles('../css/Edit.css');
+        
+            const ligi = document.querySelector('.ligi');
+            if (ligi) {
+                ligi.childNodes.forEach(node => {
+                    if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() === "||") {
+                        ligi.removeChild(node);
+                    }
+                });
             }
-        });
+        }
     }
-}
+});
